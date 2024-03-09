423a9873cedde222f0922dcdbe36f6a9cb68b91a
Kevin Devlin <azwethinkweizkd@gmail.com>
2024-03-08 20:08:58 -0700

    Update ServiceManager.sol

--- contracts/ServiceManager.sol
+++ contracts/ServiceManager.sol
@@ -3,13 +3,46 @@
 pragma solidity ^0.8.4;
 
 contract ServiceManager {
-    string[] serviceProviders;
+    mapping(address => ServiceProvider) private serviceProviders;
+    address[] private serviceProvidersIndexes;
+    enum ServiceCategory {
+        DogWalking,
+        Painting,
+        Plumbing,
+        Electrical,
+        GeneralContractor
+    }
 
-    function createNewServiceProvider(string memory _companyName) external {
-        serviceProviders.push(_companyName);
+    struct ServiceProvider {
+        address owner;
+        string companyName;
+        string email;
+        string phone;
+        uint256 serviceAmount;
+        ServiceCategory serviceCategory;
+        uint256 index;
+    }
+
+    function createNewServiceProvider(
+        string memory _companyName, 
+        string memory _email, 
+        string memory _phone, 
+        uint256 _serviceAmount,
+        ServiceCategory _serviceCategory
+        ) external {
+        serviceProvidersIndexes.push(msg.sender);
+        serviceProviders[msg.sender] = ServiceProvider({
+            owner: msg.sender,
+            companyName: _companyName,
+            email: _email,
+            phone: _phone,
+            serviceAmount: _serviceAmount,
+            serviceCategory: _serviceCategory,
+            index: serviceProvidersIndexes.length - 1
+        });
     }
 
     function getServiceProviders() external view returns (string[] memory) {
-        return serviceProviders;
+
     }
 }
