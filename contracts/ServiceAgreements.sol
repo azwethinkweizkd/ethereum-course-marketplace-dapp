5e315f2d1c1187b9cffdf838a4832b10f3732059
Kevin Devlin <azwethinkweizkd@gmail.com>
2024-03-13 21:21:01 -0600

    Service Agreement Functionality

--- contracts/ServiceAgreements.sol
+++ contracts/ServiceAgreements.sol
@@ -4,6 +4,7 @@ contract ServiceAgreement {
     address client;
     address provider;
     uint256 termsAmount;
+    WorkStatus public agreementStatus;
     constructor(
         address _client, 
         address _provider, 
@@ -12,5 +13,29 @@ contract ServiceAgreement {
         client = _client;
         provider = _provider;
         termsAmount = _termsAmount;
+        agreementStatus= WorkStatus.NotStarted;
+    }
+
+    modifier providerOnly() {
+        require(msg.sender == provider, "Only the service provider can call this.");
+        _;
+    }
+
+    enum WorkStatus {
+        NotStarted,
+        Started,
+        Completed,
+        WillNotComplete
+    }
+
+    event ServiceStatusUpdate (
+        address indexed agreementAddress,
+        WorkStatus agreementStatus
+    );
+
+    function updateServiceState(WorkStatus _status) external providerOnly {
+        agreementStatus = _status;
+
+        emit ServiceStatusUpdate(address(this), agreementStatus);
     }
 }
