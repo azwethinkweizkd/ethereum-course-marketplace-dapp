2b740143bace9831ff6749d4b43e5fb988215f2c
Kevin Devlin <azwethinkweizkd@gmail.com>
2024-03-18 14:47:41 -0600

    Successful create new service agreement

--- src/routes/serviceProviders/ServiceProvidersPage.js
+++ src/routes/serviceProviders/ServiceProvidersPage.js
@@ -1,24 +1,60 @@
 import { useEffect, useState, useRef, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 import { v4 as uuid } from "uuid";
 import ServiceProvider from "../../components/service-provider/ServiceProvider";
-import { Wrap, useToast } from "@chakra-ui/react";
+import {
+	Wrap,
+	AbsoluteCenter,
+	HStack,
+	Spinner,
+	Heading,
+	useToast,
+} from "@chakra-ui/react";
 import { useQuery } from "../shared/hooks/userQuery";
 import { useWallet } from "../../common/context/walletProvider";
 import ethereumApiFactory from "../../ethereum/ethereumApiFactory";
 import { abi, contractAddress1 } from "../../ethereum/serviceManagerContract";
 import { serviceCategoriesKeys } from "../../common/constants";
 
+const ERRORPREFIX = "VM Exception while processing transaction: revert";
+
+const errorFormat = (data) => {
+	let errorMessage;
+	if (data && data.message) {
+		const { message } = data;
+		errorMessage = message.replace(ERRORPREFIX, "");
+	}
+
+	return errorMessage;
+};
+
 const ServiceProvidersPage = () => {
 	const [serviceProviders, setServiceProviders] = useState([]);
+	const [currentBlockNumber, setCurrentBlockNumber] = useState(0);
+	const [loading, setLoading] = useState(false);
 	const { wallet } = useWallet();
 	const ethereumApi = useRef({});
 	const contractReader = useRef({});
+	const eventFilter = useRef({});
 
 	const toast = useToast();
 
 	const query = useQuery();
+
+	const navigate = useNavigate();
 	const category = query.get("category");
 	const searchCriteria = query.get("search");
+
+	const emitNewAgreementInfo = useCallback(
+		(...args) => {
+			const event = args[args.length - 1];
+			if (event.blockNumber <= currentBlockNumber) return;
+
+			setLoading(false);
+			navigate("/service-contracts");
+		},
+		[currentBlockNumber, navigate]
+	);
 
 	const updateServiceProviders = useCallback(async () => {
 		let currentServiceProviders =
@@ -45,6 +81,12 @@ const ServiceProvidersPage = () => {
 		setServiceProviders(currentServiceProviders || []);
 	}, [category, searchCriteria]);
 
+	async function updateBlockNumber() {
+		const provider = ethereumApi.current.provider;
+		const blockNumber = await provider.getBlockNumber();
+		setCurrentBlockNumber(blockNumber);
+	}
+
 	useEffect(() => {
 		if (!wallet?.accounts || !wallet?.accounts.length) return;
 
@@ -57,24 +99,78 @@ const ServiceProvidersPage = () => {
 			);
 
 			updateServiceProviders();
+			updateBlockNumber();
+
+			return () =>
+				contractReader.current.off(eventFilter, emitNewAgreementInfo);
 		} catch (error) {
 			toast({
 				title: null,
-				description: "There was an error while searching for services",
+				description: errorFormat(error),
 				status: "error",
 				duration: 9000,
 				isClosable: true,
 			});
 		}
-	}, [wallet?.accounts, toast, updateServiceProviders]);
+	}, [wallet?.accounts, toast, updateServiceProviders, emitNewAgreementInfo]);
+
+	async function handleContractAgreement(provider) {
+		const { ownerAddress } = provider;
+		if (wallet?.accounts && wallet?.accounts.length > 0) {
+			const filter = contractReader.current.filters.NewAgreement(
+				wallet.accounts[0],
+				ownerAddress
+			);
+
+			eventFilter.current = filter;
+
+			try {
+				contractReader.current.on(eventFilter, emitNewAgreementInfo);
+				setLoading(true);
+
+				return await ethereumApi.current.createServiceAgreement(ownerAddress);
+			} catch (error) {
+				toast({
+					title: null,
+					description:
+						"There was an error creating service agreement between you and the provider",
+					status: "error",
+					duration: 9000,
+					isClosable: true,
+				});
+			}
+		}
+	}
 
 	return (
-		<Wrap mt={12} spacing={24} justify="center">
-			{serviceProviders.length > 0 &&
-				serviceProviders.map((sp) => (
-					<ServiceProvider provider={sp} key={uuid()} />
-				))}
-		</Wrap>
+		<>
+			{loading ? (
+				<AbsoluteCenter>
+					<HStack alignContent="center">
+						<Spinner
+							thickness="4px"
+							speed="0.65s"
+							emptyColor="gray.200"
+							color="blue.500"
+							size="xl"
+						/>
+						<Heading>Creating new service agreement...</Heading>
+					</HStack>
+				</AbsoluteCenter>
+			) : (
+				<Wrap mt={12} spacing={24} justify="center">
+					{serviceProviders.length > 0 &&
+						serviceProviders.map((sp) => (
+							<ServiceProvider
+								provider={sp}
+								key={uuid()}
+								handleContractAgreement={handleContractAgreement}
+								loading={loading}
+							/>
+						))}
+				</Wrap>
+			)}
+		</>
 	);
 };
 
